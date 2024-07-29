e77e188503203a779e8c5bed01461d46914e1db7
Thomas Sheehan <tsheehan@indeed.com>
2024-07-29 17:44:08 +0900

    Create app, server, and utils

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+const ordersRouter = require("./orders/orders.router");
+const dishesRouter = require("./dishes/dishes.router");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/dishes", dishesRouter);
+app.use("/orders", ordersRouter);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+module.exports = app;
